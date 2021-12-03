78ec9423d8069c1c42881d7b5574fed66fc90218
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-12-03 20:05:46 +0000

    first functional component 3 a tags

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Sidebar() {
+    return (
+    <div>
+        <a>My Photos</a>
+        <a>My Illustrations</a>
+        <a>My Paintings</a>
+    </div>
+    );
+}
+
+export default Sidebar
