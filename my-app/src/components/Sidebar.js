0af35e6bf5807b3ac4dc96a6af6ba31823d3d2d1
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-12-03 20:24:25 +0000

    added css module for Sidebar component

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from "react";
+import css from './css/Sidebar.module.css';
 
 function Sidebar() {
     return (
-    <div>
+    <div className={css.sidebar}>
         <a>My Photos</a>
         <a>My Illustrations</a>
         <a>My Paintings</a>
